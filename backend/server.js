e7922cebe1d872563a73cda95cb5d70e07c2d2af
Henrique <henriquepenaconde@gmail.com>
2021-03-06 15:20:39 +0000

    start: setting up mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
@@ -26,4 +29,4 @@ app.get('/api/products/:id', (req, res) =>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} and listening at port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} and listening at port ${PORT}`.yellow.bold))
